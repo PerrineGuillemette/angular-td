3549c72cdeec5ee7476394dce0917dff9ce2c2d7
PerrineGuillemette <perrine.guillemette@gmail.com>
2017-01-30 22:47:51 +0100

    V3

--- Exo1/noteApp.js
+++ Exo1/noteApp.js
@@ -1,6 +1,38 @@
 var app = angular.module('app',[]);
-
 app.controller('NbController', [function() {
-    this.value=0;
-    this.calcul = function() { return 100-this.value; };
+    var messageNote = "";//cookie.show();
+    var info = "";
+    var self=this;
+	this.value="";
+    this.count = function() {
+        if(self.value != "" && self.value!=messageNote)
+            info = "Message modifié";
+        return 100-self.value.length;
+	};
+	this.clear = function(){
+        self.value="";
+	};
+	this.save = function(){
+		messageNote = self.value;
+        info="Message sauvegardé";
+        if (messageNote != "") {
+            cookie.enreg();
+        }
+	};
+    this.show = function(){
+
+        return info;
+    };
+}]);
+
+var cookie = angular.module('appName', ['ngCookies']);
+
+cookie.controller("ctrlCookie",["$cookies",function($cookies){
+    var favoriteCookie = "";
+    this.enreg = function() {
+            favoriteCookie = messageNote;
+    };
+    this.show = function () {
+        return favoriteCookie;
+    }
 }]);
